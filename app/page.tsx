c365318adb2f485ef28699f32e8262422f1cd3b2
Haitam Chouiekh <chouiekh@uwm.edu>
2023-03-05 16:36:04 -0600

    hehe lebeb knofne

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import Header from "./components/Header";
 import RestaurantCard from "./components/RestaurantCard";
 
 const inter = Inter({ subsets: ["latin"] });
-
 export default function Home() {
   return (
     <main>
