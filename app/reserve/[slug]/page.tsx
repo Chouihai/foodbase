57efeebfa57b5ddd91b2c935c55a8140644e2a5b
Haitam Chouiekh <chouiekh@uwm.edu>
2023-03-05 16:00:38 -0600

    bruh 2

--- app/reserve/[slug]/page.tsx
+++ app/reserve/[slug]/page.tsx
@@ -1,20 +1,13 @@
-import NavBar from "@component/app/components/NavBar";
-import Link from "next/link";
 import Form from "./components/Form";
 import Header from "./components/Header";
 
 export default function Reservation() {
   return (
-    <main className="bg-gray-100 min-h-screen w-screen">
-      <main className="max-w-screen-2xl m-auto bg-white">
-        <NavBar />
-        <div className="border-t h-screen">
-          <div className="py-9 w-3/5 m-auto">
-            <Header />
-            <Form />
-          </div>
-        </div>
-      </main>
-    </main>
+    <div className="border-t h-screen">
+      <div className="py-9 w-3/5 m-auto">
+        <Header />
+        <Form />
+      </div>
+    </div>
   );
 }
